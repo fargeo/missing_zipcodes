5b07a6bfa96fd4fb5740f0c9f28d3037b4d42bfd
Rob Gaston <rgaston@fargeo.com>
2017-08-25 15:06:27 -0700

    scripting sql generation

--- build_sql.js
+++ build_sql.js
@@ -1,31 +1,100 @@
-var fs = require('fs'); 
+var fs = require('fs');
 var parse = require('csv-parse');
 var _ = require('underscore');
 var mus = require('mustache')
 var zipcodes = [];
+var areas = {};
 
-fs.createReadStream('./new_zipcodes.csv')
-    .pipe(parse({delimiter: ','}))
-    .on('data', function(csvrow) {
-        if (csvrow[0] === 'ZipCode') {
-            return;
-        }
-        zipcodes.push({
-            ZipCode: csvrow[0],
-            WKT: csvrow[1]
-        })
-    })
-    .on('end',function() {
-        fs.readFile('./template.sql', (err, tpl) => {
-            if (err) throw err
+fs.createReadStream('./assignments.csv')
+	.pipe(parse({
+		delimiter: ','
+	}))
+	.on('data', function(csvrow) {
+		if (csvrow[0] === '' || csvrow[0] === 'Area') {
+			return;
+		}
+		if (!areas.hasOwnProperty(csvrow[0])) {
+			areas[csvrow[0]] = {
+				type: csvrow[5],
+				inspectors: [{
+					name: csvrow[3],
+					email: csvrow[4],
+					type: 'Primary',
+					areaName: csvrow[0]
+				}],
+				zipcodes: []
+			}
+		}
+		areas[csvrow[0]].zipcodes.push(csvrow[2])
+	})
+	.on('end', function() {
+		fs.createReadStream('./backup_assignments.csv')
+			.pipe(parse({
+				delimiter: ','
+			}))
+			.on('data', function(csvrow) {
+				if (csvrow[0] === '' || csvrow[0] === 'Supervisor' || !areas[csvrow[1]]) {
+					return;
+				}
+				if (csvrow[2] != '' && areas[csvrow[1]].inspectors.length > 0) {
+					areas[csvrow[1]].inspectors[0].num = csvrow[2];
+				}
+				if (csvrow[4]) {
+					areas[csvrow[1]].inspectors.push({
+						name: csvrow[5],
+						num: csvrow[4],
+						email: csvrow[6],
+						type: 'First Alternate',
+						areaName: csvrow[1]
+					})
+				}
+				if (csvrow[6]) {
+					areas[csvrow[1]].inspectors.push({
+						name: csvrow[8],
+						num: csvrow[7],
+						email: csvrow[9],
+						type: 'Second Alternate',
+						areaName: csvrow[1]
+					})
+				}
+			})
+			.on('end', function() {
+				fs.createReadStream('./new_zipcodes.csv')
+					.pipe(parse({
+						delimiter: ','
+					}))
+					.on('data', function(csvrow) {
+						if (csvrow[0] === 'ZipCode') {
+							return;
+						}
+						zipcodes.push({
+							ZipCode: csvrow[0],
+							WKT: csvrow[1]
+						})
+					})
+					.on('end', function() {
+						fs.readFile('./template.sql', (err, tpl) => {
+							if (err) throw err
+							areas = _.map(areas, function(v, k) {
+								v.zipcodes = _.map(_.unique(v.zipcodes), function(val) {
+									return {
+										areaName: k,
+										zip: val
+									};
+								});
+								v.areaName = k;
+								return v;
+							});
+							var content = mus.render(tpl.toString('utf8'), {
+								zipcodes: zipcodes,
+								areas: areas
+							});
 
-            var content = mus.render(tpl.toString('utf8'), {
-                zipcodes: zipcodes
-            });
-            
-            fs.writeFile("./output.sql", content, function(err) {
-                if(err) throw err
-            }); 
-        })
-    });
-    
+							fs.writeFile("./output.sql", content, function(err) {
+								if (err) throw err
+							});
+						})
+					});
+			});
+
+	});
--- template.sql
+++ template.sql
@@ -13,7 +13,41 @@ insert into [dbo].[GeoVector]
 		   , NULL
 		   , NULL
 		   , NULL
-           ,geometry::STGeomFromText('{{WKT}}', 3310);
+           ,geometry::STGeomFromText('{{WKT}}', 3310)
            ,'{{ZipCode}}');
 
 {{/zipcodes}}
+
+{{#areas}}
+insert into [dbo].[Area]
+        ([InspectorGroupType_ID]
+        ,[AreaName])
+    values (
+        (select [GenericLookup_ID] from [dbo].[GenericLookup] where type ='InspectorGroupType' and DisplayValue = '{{type}}')
+        ,'{{areaName}}'
+    );
+       
+{{#zipcodes}}
+insert into [dbo].[AreaZipCode]
+        ([Area_ID]
+        ,[GeoVector_ID])
+    values (
+        (select [Area_ID] from [dbo].[Area] where [AreaName] = '{{areaName}}')
+        ,(select [GeoVector_ID] from [dbo].[GeoVector] where [Name] = 'ZipCode Boundary' and [Label] = '{{zip}}')
+    );
+           
+{{/zipcodes}}
+{{#inspectors}}
+insert into [dbo].[AreaInspector]
+        ([Inspector_ID]
+        ,[Area_ID]
+        ,[AssignmentType_ID])
+    values (
+        (select [Contact_ID] from [dbo].[Contact] where [Email1] = '{{email}}' and [isEmployee] = 1)
+        ,(select [Area_ID] from [dbo].[Area] where [AreaName] = '{{areaName}}')
+        ,(select [GenericLookup_ID] from [dbo].[GenericLookup] where [Type] = 'AssignmentType' and [DisplayValue] = '{{type}}')
+    );
+
+{{/inspectors}}
+
+{{/areas}}
