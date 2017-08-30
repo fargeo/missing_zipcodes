6bce561265a2069f41160b7b01fc39dea8695ce2
Rob Gaston <rgaston@fargeo.com>
2017-08-29 17:22:05 -0700

    better js

--- build_sql.js
+++ build_sql.js
@@ -1,99 +1,113 @@
+"use strict"
 const fs = require('fs')
-const parse = require('csv-parse')
+const parse = require('csv').parse
 const _ = require('underscore')
 const mus = require('mustache')
+
 let zipcodes = []
 let areas = {}
+let content = ''
+
+const prepArea = (primaryAssignmentRow) => {
+	if (primaryAssignmentRow[0] === '' || primaryAssignmentRow[0] === 'Area') {
+		return
+	}
+	if (!areas.hasOwnProperty(primaryAssignmentRow[0])) {
+		areas[primaryAssignmentRow[0]] = {
+			type: primaryAssignmentRow[5],
+			inspectors: [{
+				name: primaryAssignmentRow[3],
+				email: primaryAssignmentRow[4],
+				type: 'Primary',
+				areaName: primaryAssignmentRow[0]
+			}],
+			zipcodes: []
+		}
+	}
+	areas[primaryAssignmentRow[0]].zipcodes.push(primaryAssignmentRow[2])
+}
+
+const getBackupAssignments = () => {
+	fs.createReadStream('./backup_assignments.csv')
+		.pipe(parse({
+			delimiter: ','
+		}))
+		.on('data', processBackupInspectorData)
+		.on('end', getZipCodeData)
+}
+
+const processBackupInspectorData = (backupInspectorData) => {
+	if (backupInspectorData[0] === '' ||
+		backupInspectorData[0] === 'Supervisor' ||
+		!areas[backupInspectorData[1]]) {
+		return
+	}
+	if (backupInspectorData[2] != '' &&
+		areas[backupInspectorData[1]].inspectors.length > 0) {
+		areas[backupInspectorData[1]].inspectors[0].num = backupInspectorData[2]
+	}
+    addBackupInspectorData(backupInspectorData, 4, 'First Alternate')
+    addBackupInspectorData(backupInspectorData, 7, 'Second Alternate')
+}
+
+const addBackupInspectorData = (backupInspectorData, startIndex, type) => {
+    if (backupInspectorData[startIndex]) {
+        areas[backupInspectorData[1]].inspectors.push({
+            name: backupInspectorData[startIndex+1],
+            num: backupInspectorData[startIndex],
+            email: backupInspectorData[startIndex+2],
+            type: type,
+            areaName: backupInspectorData[1]
+        })
+    }
+}
+
+const getZipCodeData = () => {
+    fs.createReadStream('./missing_zipcodes.csv')
+        .pipe(parse({
+            delimiter: ','
+        }))
+        .on('data', prepZipCode)
+        .on('end', writeSQLFile)
+}
+
+const prepZipCode = (zipCodeRow) => {
+    if (zipCodeRow[0] === 'ZipCode' || zipCodeRow[1] === '') {
+        return
+    }
+    zipcodes.push({
+        ZipCode: zipCodeRow[0],
+        WKT: zipCodeRow[1]
+    })
+}
+
+const writeSQLFile = () => {
+    fs.readFile('./template.sql', (err, tpl) => {
+        if (err) throw err
+        areas = _.map(areas, (v, k) => {
+            v.zipcodes = _.map(_.unique(v.zipcodes), (val) => {
+                return {
+                    areaName: k,
+                    zip: val
+                }
+            })
+            v.areaName = k
+            return v
+        })
+        content = mus.render(tpl.toString('utf8'), {
+            zipcodes: zipcodes,
+            areas: areas
+        })
+
+        fs.writeFile("./output.sql", content, (err) => {
+            if (err) throw err
+        })
+    })
+}
 
 fs.createReadStream('./assignments.csv')
 	.pipe(parse({
 		delimiter: ','
 	}))
-	.on('data', (csvrow) => {
-		if (csvrow[0] === '' || csvrow[0] === 'Area') {
-			return
-		}
-		if (!areas.hasOwnProperty(csvrow[0])) {
-			areas[csvrow[0]] = {
-				type: csvrow[5],
-				inspectors: [{
-					name: csvrow[3],
-					email: csvrow[4],
-					type: 'Primary',
-					areaName: csvrow[0]
-				}],
-				zipcodes: []
-			}
-		}
-		areas[csvrow[0]].zipcodes.push(csvrow[2])
-	})
-	.on('end', () => {
-		fs.createReadStream('./backup_assignments.csv')
-			.pipe(parse({
-				delimiter: ','
-			}))
-			.on('data', (csvrow) => {
-				if (csvrow[0] === '' || csvrow[0] === 'Supervisor' || !areas[csvrow[1]]) {
-					return
-				}
-				if (csvrow[2] != '' && areas[csvrow[1]].inspectors.length > 0) {
-					areas[csvrow[1]].inspectors[0].num = csvrow[2]
-				}
-				if (csvrow[4]) {
-					areas[csvrow[1]].inspectors.push({
-						name: csvrow[5],
-						num: csvrow[4],
-						email: csvrow[6],
-						type: 'First Alternate',
-						areaName: csvrow[1]
-					})
-				}
-				if (csvrow[6]) {
-					areas[csvrow[1]].inspectors.push({
-						name: csvrow[8],
-						num: csvrow[7],
-						email: csvrow[9],
-						type: 'Second Alternate',
-						areaName: csvrow[1]
-					})
-				}
-			})
-			.on('end', () => {
-				fs.createReadStream('./missing_zipcodes.csv')
-					.pipe(parse({
-						delimiter: ','
-					}))
-					.on('data', (csvrow) => {
-						if (csvrow[0] === 'ZipCode' || csvrow[1] === '') {
-							return
-						}
-						zipcodes.push({
-							ZipCode: csvrow[0],
-							WKT: csvrow[1]
-						})
-					})
-					.on('end', () => {
-						fs.readFile('./template.sql', (err, tpl) => {
-							if (err) throw err
-							areas = _.map(areas, (v, k) => {
-								v.zipcodes = _.map(_.unique(v.zipcodes), (val) => {
-									return {
-										areaName: k,
-										zip: val
-									}
-								})
-								v.areaName = k
-								return v
-							})
-							let content = mus.render(tpl.toString('utf8'), {
-								zipcodes: zipcodes,
-								areas: areas
-							})
-
-							fs.writeFile("./output.sql", content, (err) => {
-								if (err) throw err
-							})
-						})
-					})
-			})
-	})
+	.on('data', prepArea)
+	.on('end', getBackupAssignments)
